81a105199eaa57764568de64e7fc679af0f474f6
sabinevela <lukehemmingsteamomucho2005@gmail.com>
2025-02-13 08:15:44 -0500

    cambios

---
+++ src/main/java/com/itsqmet/nutricional/Controlador/loginNControlador.java
@@ -0,0 +1,15 @@
+package com.itsqmet.nutricional.Controlador;
+
+import com.itsqmet.nutricional.Entidad.Nutriologo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class loginNControlador {
+    @GetMapping("/loginNutri")
+    public String mostrarloginNutriologo(Model model) {
+        model.addAttribute("nutriologo", new Nutriologo()); // Agrega un objeto Usuario al modelo
+        return "login/loginNutriologo";
+    }
+}
